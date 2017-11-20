a23036e97371419afedd914e828170c42e0f904a
ArvinSiChuan <arvinsc@foxmail.com>
2017-11-20 13:28:29 +0800

    init

---
+++ old-car-sale-server/src/main/java/com/arvinsichuan/twentyseventeen/oldcarsaleserver/OldCarSaleServerApplication.java
@@ -0,0 +1,12 @@
+package com.arvinsichuan.twentyseventeen.oldcarsaleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldCarSaleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldCarSaleServerApplication.class, args);
+	}
+}
